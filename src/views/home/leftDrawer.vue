b0b8b645bc911d873509108a8619073ca30fbc80
慕卿子 <>
2022-03-20 11:40:59 +0800

    提交

--- src/views/home/leftDrawer.vue
+++ src/views/home/leftDrawer.vue
@@ -4,96 +4,104 @@
       <div style="margin-bottom: 8px; height: calc(33% - 8px)">
         <v-card>
           <v-title>渔港信息</v-title>
-          <div class="admin">
-            <el-row>
-              <el-col :span="12">
-                <img src="~images/store/point.png" />
-                渔港等级：
-                <span>二级</span>
-              </el-col>
 
-              <el-col :span="12">
-                <img src="~images/store/point.png" />
-                渔港类型：
-                <span>综合性港口</span>
-              </el-col>
-            </el-row>
+          <div class="swipers">
+            <img
+              src="~images/store/prev.png"
+              class="elprev elcBtn"
+              @click="swprev('mySsSwiper')"
+            />
+            <img
+              src="~images/store/next.png"
+              class="elnext elcBtn"
+              @click="swnext('mySsSwiper')"
+            />
+            <swiper :options="swiperOption" ref="mySsSwiper">
+              <swiper-slide class="swiper-slides ss-swiper-slides">
+                <div class="admin">
+                  <div class="adminLeft">
+                    <img src="~images/home/icon_yg.png" />
+                  </div>
+                  <div class="adminRight">
+                    <p>
+                      <img src="~images/store/point.png" />
+                      渔港等级：
+                      <span>二级</span>
+                    </p>
+                    <p>
+                      <img src="~images/store/point.png" />
+                      渔港类型：
+                      <span>综合性港口</span>
+                    </p>
+                    <p>
+                      <img src="~images/store/point.png" />
+                      所在地：
+                      <span>广东省湛江市霞山区渔人码头</span>
+                    </p>
+                    <p>
+                      <img src="~images/store/point.png" />
+                      负责港长：
+                      <span>刘宏杰</span>
+                    </p>
+                    <p>
+                      <img src="~images/store/point.png" />
+                      联系方式：
+                      <span>13702843239</span>
+                    </p>
+                    <p>
+                      <img src="~images/store/point.png" />
+                      驻港机构：
+                      <span>霞山渔政大队</span>
+                    </p>
 
-            <el-row>
-              <el-col :span="24">
-                <img src="~images/store/point.png" />
-                所在地：
-                <span>广东省湛江市霞山区渔人码头</span>
-              </el-col>
-            </el-row>
+                    <p>
+                      <img src="~images/store/point.png" />
+                      机构负责人：
+                      <span>刘镇</span>
+                    </p>
+                    <p>
+                      <img src="~images/store/point.png" />
+                      联系方式：
+                      <span>0750-5311601</span>
+                    </p>
+                  </div>
+                </div>
+              </swiper-slide>
 
-            <el-row>
-              <el-col :span="12">
-                <img src="~images/store/point.png" />
-                负责港长：
-                <span>刘宏杰</span>
-              </el-col>
-              <el-col :span="12">
-                <img src="~images/store/point.png" />
-                联系方式：
-                <span>13702843239</span>
-              </el-col>
-            </el-row>
+              <swiper-slide class="swiper-slides ss-swiper-slides">
+                <div class="admin">
+                  <div class="adminLeft">
+                    <img src="~images/store/adminHeader.png" />
+                  </div>
+                  <div class="adminRight">
+                    <p>
+                      <img src="~images/store/point.png" />
+                      值班人：
+                      <span>胡小栗</span>
+                    </p>
+                    <p>
+                      <img src="~images/store/point.png" />
+                      值班时间：
+                      <span>08:00~18:00</span>
+                    </p>
 
-            <el-row>
-              <el-col :span="24">
-                <img src="~images/store/point.png" />
-                驻港机构：
-                <span>霞山渔政大队</span>
-              </el-col>
-            </el-row>
-
-            <el-row>
-              <el-col :span="12">
-                <img src="~images/store/point.png" />
-                机构负责人：
-                <span>刘镇</span>
-              </el-col>
-              <el-col :span="12">
-                <img src="~images/store/point.png" />
-                联系方式：
-                <span>0750-5311601</span>
-              </el-col>
-            </el-row>
-
-            <el-row>
-              <el-col :span="12">
-                <img src="~images/store/point.png" />
-                水域面积(万平方米)：
-                <span>11.12</span>
-              </el-col>
-              <el-col :span="12">
-                <img src="~images/store/point.png" />
-                年卸货量(万吨)：
-                <span>2.5</span>
-              </el-col>
-            </el-row>
-
-            <el-row>
-              <el-col :span="12">
-                <img src="~images/store/point.png" />
-                在港渔船(艘)：
-                <span>105</span>
-              </el-col>
-              <el-col :span="12">
-                <img src="~images/store/point.png" />
-                码头长度(米)：
-                <span>215</span>
-              </el-col>
-            </el-row>
+                    <p>
+                      <img src="~images/store/point.png" />
+                      联系方式：
+                      <span>13702843239</span>
+                    </p>
+                  </div>
+                </div>
+              </swiper-slide>
+            </swiper>
           </div>
         </v-card>
       </div>
       <div style="height: calc(34% - 8px); margin-bottom: 8px">
         <v-card>
-          <el-button class="detailBtn" @click="handleClick" type="text"
-            >详情>>></el-button
-          >
+          <el-button class="detailBtn" @click="handleClick" type="text">
+            更多>>>
+          </el-button>
           <v-title>进出港记录</v-title>
           <div class="panelContent">
             <div class="head">
@@ -136,48 +144,15 @@
         </v-card>
       </div>
     </v-drawer>
-
-    <el-dialog
-      title="进出港记录"
-      :visible.sync="dialogVisible"
-      class="dialogDetail"
-      width="80%"
-      append-to-body
-    >
-      <el-tabs v-model="tabValue" @tab-click="handleClickQxz">
-        <el-tab-pane label="进港" name="in">
-          <el-row :gutter="20">
-            <el-col :span="8">
-              <el-card class="box-card">
-                <div v-for="o in 4" :key="o" class="text item">
-                  {{ "列表内容 " + o }}
-                </div>
-              </el-card>
-            </el-col>
-            <el-col :span="8">
-              <el-card class="box-card">
-                <div v-for="o in 4" :key="o" class="text item">
-                  {{ "列表内容 " + o }}
-                </div>
-              </el-card>
-            </el-col>
-            <el-col :span="8">
-              <el-card class="box-card">
-                <div v-for="o in 4" :key="o" class="text item">
-                  {{ "列表内容 " + o }}
-                </div>
-              </el-card>
-            </el-col>
-          </el-row>
-        </el-tab-pane>
-        <el-tab-pane label="出港" name="out"> </el-tab-pane>
-      </el-tabs>
-    </el-dialog>
   </div>
 </template>
 
 <script>
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import "swiper/dist/css/swiper.css";
+
 export default {
+  components: { Swiper, SwiperSlide },
   computed: {
     // 公告滚动自定义
     defaultOption() {
@@ -195,13 +170,16 @@ export default {
   },
   data() {
     return {
-      imgList: [
-        require("images/map/gdhc.png"),
-        require("images/map/gpqf.png"),
-        require("images/map/my.png"),
-        require("images/map/ssy.png"),
-        require("images/map/stj.png"),
-      ],
+      swiperOption: {
+        observer: true,
+        observeParents: true,
+        debugger: true,
+        autoplayDisableOnInteraction: false,
+        slidesPerView: 1,
+        paginationClickable: true,
+        centeredSlides: true,
+        autoplay: 5000,
+      },
       jcgRecord: [
         { code: "粤渔8421", statu: "出港", date: "2022-03-11 05:20:23" },
         { code: "粤渔2521", statu: "出港", date: "2022-03-12 05:22:54" },
@@ -211,20 +189,22 @@ export default {
       ],
       yjList: [
         {
-          content: "粤渔1546提前回港，请注意！",
+          content: "2022-02-16 08:56:07，粤渔1546未报备进港！",
           date: "2022-02-16 08:56:07",
           id: "ff80808179087e2b01795b121b5d3866",
         },
       ],
-      dialogVisible: false,
-      tabValue: "in",
     };
   },
 
   methods: {
-    handleClick() {
-      this.dialogVisible = true;
+    swprev(ref) {
+      this.$refs[ref].$swiper.slidePrev();
     },
+    swnext(ref) {
+      this.$refs[ref].$swiper.slideNext();
+    },
+    handleClick() {},
   },
   destroyed() {},
 };
@@ -232,28 +212,64 @@ export default {
 
 <style lang="stylus" scoped>
 .panelContent
-  font-size 0.08rem
+  font-size .08rem
   height calc(100% - 43px)
   color #fff
 
-.admin
+.swipers
+  width 100%
   height calc(100% - 43px)
-  padding 0 10px
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
+  position relative
 
-  >>>.el-col
+  .elcBtn
+    z-index 999
+    position absolute
+    cursor pointer
+
+  .elprev
+    top 50%
+    transform translate(100%, -50%)
+    right 100%
+
+  .elnext
+    top 50%
+    transform translate(-100%, -50%)
+    left 100%
+
+  >>>.swiper-container
+    height 100%
+
+.admin
+  height 100%
+  display flex
+  justify-content space-around
+
+  .adminLeft
+    height 100%
     display flex
     align-items center
-    font-size .08rem
-    color #27f7ec
 
     img
-      margin-right .05rem
+      width .6rem
 
-    span
-      color #fff
+  .adminRight
+    width calc(100% - 1rem)
+    height 100%
+    display flex
+    flex-direction column
+    justify-content space-around
+
+    p
+      display flex
+      align-items center
+      font-size .08rem
+      color #27f7ec
+
+      img
+        margin-right .05rem
+
+      span
+        color #fff
 
 .head
   height .15rem
@@ -265,7 +281,7 @@ export default {
 
   p
     text-align center
-    line-height 0.16rem
+    line-height .16rem
 
 .body
   display flex
@@ -284,7 +300,6 @@ export default {
 
   p:nth-child(3)
     width 40%
-
 
 .seamless-warp
   height 100%
@@ -338,56 +353,4 @@ export default {
 .detailBtn
   position absolute
   right 10px
-</style>
-
-<style lang="stylus">
-.dialogDetail
-  .el-dialog
-    background url('~images/common/card.png')
-    background-repeat no-repeat
-    background-size 100% 100%
-
-    .el-dialog__header
-      padding: 15px 15px 0;
-
-    .el-dialog__title
-      background-image -webkit-linear-gradient(top, #fff, #A1C7E8, #7BBEF6)
-      -webkit-background-clip text
-      -webkit-text-fill-color transparent
-      font-size 0.09rem
-      line-height 0.15rem
-      font-weight 600
-
-    .el-dialog__body
-      padding 0 20px 20px
-
-    .el-dialog__headerbtn .el-dialog__close
-      color #fff
-
-    .el-tabs
-      height 100%
-
-      .el-tabs__header
-        margin 0 0 10px
-
-      .el-tabs__content
-        height calc(100% - 38px)
-
-      .el-tab-pane
-        height 100%
-
-      .el-tabs__item
-        color #fff
-        height 28px
-        line-height 28px
-        padding 0 15px
-
-      .el-tabs__item.is-active
-        color rgb(243, 208, 19)
-
-      .el-tabs__nav-wrap::after
-        height 0
-
-      .el-tabs__active-bar
-        background rgb(243, 208, 19)
 </style>
